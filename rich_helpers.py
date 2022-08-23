8c3df9ae2cea5cec6d194ac7b8d9d302b3d1a429
Daniel Barrientos <danielbarrientos68@gmail.com>
2022-08-23 15:32:28 -0300

    table_maker

--- rich_helpers.py
+++ rich_helpers.py
@@ -1,6 +1,9 @@
 from rich.console import Console
 from rich.panel import Panel
 from rich.text import Text
+from rich.prompt import Prompt
+from rich.table import Table
+from rich import box
 
 
 def title():
@@ -8,8 +11,28 @@ def title():
     console = Console()
     console.print(title, style="italic bright_red", justify='center')
 
+
 def description():
     description = Text("\nThe dataset contains info on all the comic books ever released in the Marvel Universe. You can search for your favorite writer, your favorite series or the comics that came out during your childhood.\n")
     description.stylize("bold magenta")
     console = Console()
     console.print(description)
+
+
+def choose_option():
+    option = Prompt.ask('[1] Search for a writer, [2] Search for a series, [3] Search for a comic book, [4] Search for a Year of activity, [5] Exit', choices=[
+                        "1", "2", "3", "4", "5"], show_choices=False)
+    return option
+
+
+def table_maker(data, title):
+    table = Table(title=title.title(), show_header=True,
+                  header_style="bold magenta", box=box.DOUBLE_EDGE, show_lines=True)
+    for col in data[0]._fields:
+        table.add_column(col.replace("_", " ").title())
+
+    for row in data:
+        table.add_row(*row)
+
+    console = Console()
+    console.print(table)
--- test_rich.py
+++ test_rich.py
@@ -2,5 +2,7 @@ import rich_helpers as r
 import load as l
 r.title()
 r.description()
+option = r.choose_option()
 MARVEL = l.load_csv()
-
+print(option)
+r.table_maker([MARVEL[100],MARVEL[101], MARVEL[102]], "Comic Name")
